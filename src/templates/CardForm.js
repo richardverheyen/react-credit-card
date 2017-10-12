7b0dde7b911dce93d82e0c49726550025c195760
richardverheyen <richard.verheyen@gmail.com>
2017-10-12 15:46:29 -0700

    Add full form styles for desktop view

--- src/templates/CardForm.js
+++ src/templates/CardForm.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
 
 class CardForm extends Component {
+
+
   render() {
     return (
-      <form>
-          <div>
-            <button data-value="5">$5</button>
-            <button data-value="10">$10</button>
-            <button data-value="20">$20</button>
-            <button data-value="40">$40</button>
-            <button data-value="100">$100</button>
-            <button data-value="250">$250</button>
-            <button data-value="1000">$1000</button>
+      <form id="card-form">
+          <div id="values">
+            <button className="button" data-value="5">$5</button>
+            <button className="button" data-value="10">$10</button>
+            <button className="button" data-value="20">$20</button>
+            <button className="button green" data-value="40">$40</button>
+            <button className="button" data-value="100">$100</button>
+            <button className="button" data-value="250">$250</button>
+            <button className="button" data-value="1000">$1000</button>
           </div>
-          <input id="cardNumber" type="text" className="form-field" defaultValue="Credit card number"/>
+          <input id="card-number" type="text" className="form-field" defaultValue="Credit card number"/>
           <div>
               <input id="owner" type="text" className="form-field" defaultValue="Name on card"/>
               <input id="expiry-date" type="text" className="form-field" defaultValue="MM/YY"/>
               <input id="cvv" type="text" className="form-field" defaultValue="CVC"/>
           </div>
-          <button id="confirm-purchase" type="submit" className="btn green">Deposit funds</button>
+          <button id="confirm-purchase" type="submit" className="button green">Deposit funds</button>
       </form>
     );
+  }
+
+  componentDidMount(){
+    const valueButtons = document.getElementById('values');
+    valueButtons.childNodes.forEach(node => node.addEventListener("click", function(e){
+      e.preventDefault();
+      this.className += " green";
+    }));
+
   }
 }
 
