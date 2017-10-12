172be318e750b186599639d95eb2ed2c64dcf9d1
richardverheyen <richard.verheyen@gmail.com>
2017-10-12 12:27:33 -0700

    Add transfer value buttons to form

--- src/templates/CardForm.js
+++ src/templates/CardForm.js
@@ -4,6 +4,15 @@ class CardForm extends Component {
   render() {
     return (
       <form>
+          <div>
+            <button data-value="5">$5</button>
+            <button data-value="10">$10</button>
+            <button data-value="20">$20</button>
+            <button data-value="40">$40</button>
+            <button data-value="100">$100</button>
+            <button data-value="250">$250</button>
+            <button data-value="1000">$1000</button>
+          </div>
           <input id="cardNumber" type="text" className="form-field" defaultValue="Credit card number"/>
           <div>
               <input id="owner" type="text" className="form-field" defaultValue="Name on card"/>
