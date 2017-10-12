bf6cc53c571f050e7e0fa0456c64c0abdb2d53d8
richardverheyen <richard.verheyen@gmail.com>
2017-10-12 12:15:01 -0700

    Add form content for CardForm component

--- src/templates/CardForm.js
+++ src/templates/CardForm.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class CardForm extends Component {
   render() {
     return (
-      <h1>Form</h1>
+      <form>
+          <input id="cardNumber" type="text" className="form-field" defaultValue="Credit card number"/>
+          <div>
+              <input id="owner" type="text" className="form-field" defaultValue="Name on card"/>
+              <input id="expiry-date" type="text" className="form-field" defaultValue="MM/YY"/>
+              <input id="cvv" type="text" className="form-field" defaultValue="CVC"/>
+          </div>
+          <button id="confirm-purchase" type="submit" className="btn green">Deposit funds</button>
+      </form>
     );
   }
 }
